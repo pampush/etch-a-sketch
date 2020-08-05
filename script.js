5edc8a6c8f6862e188529a03585cce49c2bd98e8
pampush <32041268+pampush@users.noreply.github.com>
2020-08-06 01:49:25 +0300

    add event delegation for grid pixels

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// bad code 
 "use strict";
 function Grid() {
-  this.container = document.querySelector('.grid-container__sketch'); 
+  this.container = document.querySelector('.grid-container__sketch');
   this.pixels = this.container.querySelectorAll('div'); // try children
   this.mode = 'greyscale';
   this.size = 3;
-  this.method = null;
+  this.method = 'greyscale';
 
   this.resetcssText = function() {
-    this.pixels.forEach(node => { node.style.cssText=""; node.classList.remove('grid-container__sketch-hover')}); 
+    this.pixels.forEach(node => { node.style.cssText=''; node.dataset.opacity='0'; node.classList.remove('grid-container__sketch-hover')}); 
   }
 
   this.update = function () {
-    this.container = document.querySelector('.grid-container__sketch'); 
-    this.container.removeEventListener('mouseover', (e) => { let target = e.target; method(mode, target);});
     this.pixels = this.container.querySelectorAll('div');
   }
 
@@ -27,40 +24,40 @@ function Grid() {
       node = document.createElement('div');
       this.container.appendChild(node);
     }
-    
     this.update();
-    //this.pixels.forEach(elem => elem.addEventListener('mouseover', method(mode)));
-    this.container.addEventListener('mouseover', (e) => { let target = e.target; if(!this.method) return; this.method(target);})
-  }
-  
-  
- /*  this.pixelDraw = function (mode) {
+  }  
+
+  this.colorful = function(target) {
     function randomColor() { 
       return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
     }
-    return function() { 
-      this.classList.toggle('grid-container__sketch-hover'); // 3d rotation
-      if(mode == 'greyscale') {
-          if(this.dataset.opacity)
-            this.dataset.opacity =`${+(this.dataset.opacity) + 0.1}` ;
-          else
-            this.dataset.opacity = '0.1';
-      
-          this.style.backgroundColor = `rgba(0, 0, 0, ${this.dataset.opacity})`;
-      } 
-      else
-          this.style.backgroundColor = `${randomColor()}`;
-      
-      this.style.border = '0px'; // somehow border: 0px; slows grid with 30+ pixels down (dev mode issue)
-    }
-  } */
- 
+    target.classList.toggle('grid-container__sketch-hover'); // 3d rotation
+    target.style.backgroundColor = `${randomColor()}`;   
+    target.style.border = '0px'; // somehow border: 0px; slows grid with 30+ pixels down (dev mode issue)  
+  };
+
+  this.greyscale = function(target) {
+    if(target.dataset.opacity)
+      target.dataset.opacity =`${+(target.dataset.opacity) + 0.1}` ;
+    else
+      target.dataset.opacity = '0.1';
+    target.style.border = '0px';
+    target.style.backgroundColor = `rgba(0, 0, 0, ${target.dataset.opacity})`;
+  };
+
+  this.onMouseOver = function(e) {
+    if(!this[this.method] || e.target.classList == 'grid-container__sketch') 
+      return; 
+    this[this.method](e.target);
+  };
+
+  this.container.addEventListener('mouseover', this.onMouseOver.bind(this)); 
   this.update = this.update.bind(this);  
   this.resetcssText = this.resetcssText.bind(this);
   this.gridInit = this.gridInit.bind(this);
 } 
   
-const colorfulDraw = function (target) {
+/* const colorfulDraw = function (target) {
   function randomColor() { 
     return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
   }
@@ -76,20 +73,21 @@ const greyscaleDraw = function(target) {
     target.dataset.opacity = '0.1';
 
   target.style.backgroundColor = `rgba(0, 0, 0, ${target.dataset.opacity})`;
-}
+} */
 
-  const button = document.querySelector("input[name='submit-button']"),
-        modeNodes = document.querySelectorAll("input[name='mode']"),
-        sizeElem = document.querySelector("input[type='number']"),
-        res = document.querySelector("input[name='reset']"),
-        colorfulNode = document.querySelector("input[value='colorful']"),
-        greyscaleNode = document.querySelector("input[value='greyscale']"),
-        grid = new Grid(); 
-  
-  button.addEventListener('click', () => {  grid.size = sizeElem.value; grid.gridInit(pixelDraw);});
-  
-  colorfulNode.addEventListener('click', () => { grid.mode = 'colorful'; grid.resetcssText(); grid.method = colorfulDraw});   
-  greyscaleNode.addEventListener('click', () => { grid.mode = 'greyscale'; grid.resetcssText(); grid.method = greyscaleDraw}); 
-  res.addEventListener('click', () => grid.resetcssText);
-  grid.method = colorfulDraw;
-  grid.gridInit();
+const button = document.querySelector("input[name='submit-button']"),
+      modeNodes = document.querySelectorAll("input[name='mode']"),
+      sizeElem = document.querySelector("input[type='number']"),
+      res = document.querySelector("input[name='reset']"),
+      switchModeNode = document.querySelector(".switch-mode"),
+      grid = new Grid(); 
+
+button.addEventListener('click', () => {  grid.size = sizeElem.value; grid.gridInit();});
+
+switchModeNode.addEventListener('click', (e) => { 
+  if(e.target && e.target.tagName == 'INPUT') 
+    grid.method = e.target.value; 
+  grid.resetcssText()})
+
+res.addEventListener('click', grid.resetcssText);
+grid.gridInit();  
